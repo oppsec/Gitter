6258f2c04039f1f2989c81c4c6c3426b826ea8e8
oppsec <daniel.sdm.github@gmail.com>
2021-05-28 12:16:22 -0300

    fix: fixed account url error

--- src/core.py
+++ src/core.py
@@ -7,7 +7,7 @@ from rich import print
 from src.messages import input_name, error, success
 
 class Account:
-    def __init__(self, url, name, repos, gists, followers, following) -> None:
+    def __init__(self, name, url, repos, gists, followers, following) -> None:
         self.name = name
         self.url = url
         self.repos = repos
@@ -18,7 +18,6 @@ class Account:
 
 def get_username() -> None:
     """ Define Github account name """
-
     username_input = input(input_name)
     connect(username_input)
 
@@ -28,27 +27,25 @@ def connect(username_input) -> None:
     Connect to the GitHub API and
     confirm if the account exists
     """
-
     github_api_url = f'https://api.github.com/users/{username_input}'
     response = requests.get(github_api_url, verify=False, timeout=10, allow_redirects=False)
 
-    get_information(response, github_api_url) if response.status_code == 200 else print(error)
+    get_information(response) if response.status_code == 200 else print(error)
 
 
-def get_information(response, github_api_url) -> None:
+def get_information(response) -> None:
     """ Capture the JSON response and get the account informations """
-
     print(success)
 
     dump = response.json()
-    account = Account(github_api_url, dump['login'], dump['public_repos'], dump['public_gists'], dump['followers'], dump['following'])
+    account = Account(dump['login'], dump['html_url'], dump['public_repos'], dump['public_gists'], dump['followers'], dump['following'])
     
     result(account)
 
 
 def result(account) -> str:
     """ Returns the informations """
-
+    
     print(f"""[bold yellow]
 * Account Name: {account.name}
 * Account URL: {account.url}
