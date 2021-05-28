ff6bd55cf635970358ea9ca5567a1e64077007f4
oppsec <daniel.sdm.github@gmail.com>
2021-05-28 12:22:19 -0300

    dev: added function __docs__

--- src/ui.py
+++ src/ui.py
@@ -5,10 +5,12 @@ def banner():
     """
     Returns the application banner
     """
-
     with open('src/banner.txt') as file:
         content = file.read()
         return print(f"[bold yellow]{content}[/]")
 
 def clear():
+    """
+    Clear the screen to spawn the banner
+    """
     system('cls' if name == 'nt' else 'clear')
